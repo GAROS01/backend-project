431c303bbd0cc418ebd11f0993201dd68a3a4c07
GAROSDEV <garzonoscar7@gmail.com>
2024-08-18 17:35:00 -0500

    feat: Update getClients and getClient controllers to use query and format functions

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,10 +1,11 @@
 import { pool } from "../db.js";
+import { queryGetClients, queryGetClient } from "./query.js";
+import { formatClients } from "./format.controllers.js";
 
 export const getClients = async (req, res) => {
   try {
-    const rows = await pool.query("SELECT * FROM clientes");
-    console.log(rows[0]);
-    res.json(rows[0]);
+    const rows = await pool.query(queryGetClients);
+    res.json(formatClients(rows[0]));
   } catch (error) {
     res.status(500);
     console.log(error);
@@ -14,14 +15,11 @@ export const getClients = async (req, res) => {
 export const getClient = async (req, res) => {
   try {
     const { id } = req.params;
-    const rows = await pool.query(
-      "SELECT * FROM clientes WHERE id_cliente = ?",
-      [id]
-    );
+    const rows = await pool.query(queryGetClient, [id]);
     if (rows[0].length === 0) {
       return res.status(404).json({ message: "Cliente no encontrado" });
     }
-    res.json(rows[0]);
+    res.json(formatClients(rows[0])[0]);
   } catch (error) {
     res.status(500);
     console.log(error);
@@ -30,20 +28,12 @@ export const getClient = async (req, res) => {
 
 export const createClient = async (req, res) => {
   try {
-    console.log(req.body);
     const { nombre, apellido, telefono, creador_administrativo } = req.body;
-    console.log(nombre, apellido, telefono, creador_administrativo);
     const rows = await pool.query(
       "INSERT INTO clientes (nombre, apellido, telefono, creador_administrativo) VALUES (?,?,?,?)",
       [nombre, apellido, telefono, creador_administrativo]
     );
-    res.json({
-      id: rows.insertId,
-      nombre,
-      apellido,
-      telefono,
-      creador_administrativo,
-    });
+    res.json({ message: "Cliente creado", id: rows[0].insertId });
   } catch (error) {
     res.status(500);
     console.log(error);
@@ -52,7 +42,6 @@ export const createClient = async (req, res) => {
 
 export const updateClient = async (req, res) => {
   try {
-    console.log(req.body);
     const { nombre, apellido, telefono } = req.body;
     const { id } = req.params;
     const rows = await pool.query(
@@ -62,6 +51,7 @@ export const updateClient = async (req, res) => {
     if (rows[0].affectedRows === 0) {
       return res.status(404).json({ message: "Cliente no encontrado" });
     }
+    res.json({ message: "Cliente actualizado" });
   } catch (error) {
     res.status(500);
     console.log(error);
