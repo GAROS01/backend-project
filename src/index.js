b61682ad83875e2aeaaac929066011f51de98dcb
GAROSDEV <garzonoscar7@gmail.com>
2024-09-06 22:17:07 -0500

    Agrego a morgan para testeo de la api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import PORT from "./config.js";
 import clientesRoutes from "./routes/clientes.routes.js";
 import adminRoutes from "./routes/administrativos.routes.js";
@@ -6,12 +7,15 @@ import manicuristasRoutes from "./routes/manicuristas.routes.js";
 import citasRoutes from "./routes/citas.routes.js";
 
 const app = express();
+app.use(morgan("tiny"));
 
 app.use(express.json());
 app.use("/api", clientesRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", manicuristasRoutes);
 app.use("/api", citasRoutes);
+
+morgan(":method :url :status :res[content-length] - :response-time ms");
 
 app.listen(PORT);
 console.log("Server on port", PORT);
