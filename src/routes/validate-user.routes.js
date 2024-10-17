0b72f98a68e5dbf6c9f91488b5afbbda1d2fab0a
GAROSDEV <garzonoscar7@gmail.com>
2024-10-17 12:57:58 -0500

    Refactorizar controladores para incluir campo de correo electrónico en la creación de clientes y manicuristas

--- src/routes/validate-user.routes.js
+++ src/routes/validate-user.routes.js
@@ -1,12 +1,32 @@
 import { Router } from "express";
 import validateToken from "../supabase/auth.js";
+import { pool } from "../db.js";
 
 const router = Router();
 
-router.post("/validate-user", validateToken, (req, res) => {
+router.post("/validate-user", validateToken, async (req, res) => {
   const user = req.user;
   console.log(user);
-  res.json({ correo: user.email, nombre: user.user_metadata.name });
+
+  // Extrae el correo y el nombre del objeto user
+  const correo = user.email;
+  const nombre = user.user_metadata.name;
+
+  if (!correo || !nombre) {
+    return res.status(400).json({ error: "Correo y nombre son requeridos" });
+  }
+
+  try {
+    // Inserta los datos en la base de datos
+    const rows = await pool.query(
+      "INSERT INTO clientes (correo, nombre) VALUES (?,?)",
+      [correo, nombre]
+    );
+    res.json({ correo, nombre });
+  } catch (error) {
+    console.error("Error al insertar en la base de datos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 });
 
 export default router;
