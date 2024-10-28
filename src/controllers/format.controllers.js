e5f7e5b4591ab3d2de68e4be330436b241d2087d
GAROSDEV <garzonoscar7@gmail.com>
2024-10-28 15:57:59 -0500

    Refactorizar controladores para incluir campos de correo electrónico y contraseña en las tablas administrativos, clientes y manicuristas

--- src/controllers/format.controllers.js
+++ src/controllers/format.controllers.js
@@ -1,38 +1,3 @@
-export const formatClients = (rows) => {
-  return rows.map((row) => ({
-    id: row.id_cliente,
-    correo: row.cliente_correo,
-    nombre: row.cliente_nombre,
-    apellido: row.cliente_apellido,
-    telefono: row.cliente_telefono,
-    fecha_registro: row.fecha_registro,
-    creador_administrativo: {
-      id: row.admin_id,
-      correo: row.admin_correo,
-      nombre: row.admin_nombre,
-      apellido: row.admin_apellido,
-      telefono: row.admin_telefono,
-    },
-  }));
-};
-
-export const formatManicuristas = (rows) => {
-  return rows.map((row) => ({
-    id_manicurista: row.id_manicurista,
-    correo: row.manicurista_correo,
-    nombre: row.manicurista_nombre,
-    apellido: row.manicurista_apellido,
-    telefono: row.manicurista_telefono,
-    creado_por_administrativo: {
-      id_administrativo: row.id_administrativo,
-      correo: row.admin_correo,
-      nombre: row.admin_nombre,
-      apellido: row.admin_apellido,
-      telefono: row.admin_telefono,
-    },
-  }));
-};
-
 export const formatCitas = (rows) => {
   return rows.map((row) => ({
     id_cita: row.id_cita,
@@ -42,23 +7,14 @@ export const formatCitas = (rows) => {
     id_cliente: {
       id_cliente: row.id_cliente,
       correo: row.cliente_correo,
-      nombre: row.cliente_nombre,
-      apellido: row.cliente_apellido,
+      nombre_completo: row.cliente_nombre_completo,
       telefono: row.cliente_telefono,
     },
     id_manicurista: {
       id_manicurista: row.id_manicurista,
       correo: row.manicurista_correo,
-      nombre: row.manicurista_nombre,
-      apellido: row.manicurista_apellido,
+      nombre_completo: row.manicurista_nombre_completo,
       telefono: row.manicurista_telefono,
-    },
-    gestiona_administrativo: {
-      id_administrativo: row.id_administrativo,
-      correo: row.admin_correo,
-      nombre: row.admin_nombre,
-      apellido: row.admin_apellido,
-      telefono: row.admin_telefono,
     },
   }));
 };
--- src/controllers/query.js
+++ src/controllers/query.js
@@ -1,68 +1,3 @@
-export const queryGetClients = `SELECT 
-        c.id_cliente, 
-        c.correo AS cliente_correo,
-        c.nombre AS cliente_nombre, 
-        c.apellido AS cliente_apellido, 
-        c.telefono AS cliente_telefono, 
-        c.fecha_registro, 
-        a.id_administrativo AS admin_id, 
-        a.correo AS admin_correo,
-        a.nombre AS admin_nombre, 
-        a.apellido AS admin_apellido, 
-        a.telefono AS admin_telefono
-    FROM clientes c
-    JOIN administrativos a ON c.creador_administrativo = a.id_administrativo;
-`;
-
-export const queryGetClient = `SELECT 
-        c.id_cliente, 
-        c.correo AS cliente_correo,
-        c.nombre AS cliente_nombre, 
-        c.apellido AS cliente_apellido, 
-        c.telefono AS cliente_telefono, 
-        c.fecha_registro, 
-        a.id_administrativo AS admin_id, 
-        a.correo AS admin_correo,
-        a.nombre AS admin_nombre, 
-        a.apellido AS admin_apellido, 
-        a.telefono AS admin_telefono
-      FROM clientes c
-      JOIN administrativos a ON c.creador_administrativo = a.id_administrativo
-      WHERE c.id_cliente = ?`;
-
-export const queryGetManicuristas = `
-      SELECT 
-        m.id_manicurista, 
-        m.correo AS manicurista_correo,
-        m.nombre AS manicurista_nombre, 
-        m.apellido AS manicurista_apellido, 
-        m.telefono AS manicurista_telefono, 
-        a.id_administrativo, 
-        a.correo AS admin_correo,
-        a.nombre AS admin_nombre, 
-        a.apellido AS admin_apellido, 
-        a.telefono AS admin_telefono
-      FROM manicuristas m
-      JOIN administrativos a ON m.creado_por_administrativo = a.id_administrativo;
-    `;
-
-export const queryGetManicurista = `
-      SELECT 
-        m.id_manicurista, 
-        m.correo AS manicurista_correo,
-        m.nombre AS manicurista_nombre, 
-        m.apellido AS manicurista_apellido, 
-        m.telefono AS manicurista_telefono, 
-        a.id_administrativo, 
-        a.correo AS admin_correo,
-        a.nombre AS admin_nombre, 
-        a.apellido AS admin_apellido, 
-        a.telefono AS admin_telefono
-      FROM manicuristas m
-      JOIN administrativos a ON m.creado_por_administrativo = a.id_administrativo
-      WHERE m.id_manicurista = ?;
-    `;
-
 export const queryGetCitas = `
       SELECT 
         c.id_cita, 
@@ -71,23 +6,15 @@ export const queryGetCitas = `
         c.fecha_creacion, 
         cl.id_cliente, 
         cl.correo AS cliente_correo,
-        cl.nombre AS cliente_nombre, 
-        cl.apellido AS cliente_apellido, 
+        cl.nombre_completo AS cliente_nombre_completo, 
         cl.telefono AS cliente_telefono, 
         m.id_manicurista, 
         m.correo AS manicurista_correo,
-        m.nombre AS manicurista_nombre, 
-        m.apellido AS manicurista_apellido, 
-        m.telefono AS manicurista_telefono, 
-        a.id_administrativo, 
-        a.correo AS admin_correo,
-        a.nombre AS admin_nombre, 
-        a.apellido AS admin_apellido, 
-        a.telefono AS admin_telefono
+        m.nombre_completo AS manicurista_nombre_completo, 
+        m.telefono AS manicurista_telefono
       FROM citas c
       JOIN clientes cl ON c.id_cliente = cl.id_cliente
-      JOIN manicuristas m ON c.id_manicurista = m.id_manicurista
-      JOIN administrativos a ON c.gestiona_administrativo = a.id_administrativo;
+      JOIN manicuristas m ON c.id_manicurista = m.id_manicurista;
     `;
 
 export const queryGetCita = `
@@ -98,22 +25,14 @@ export const queryGetCita = `
         c.fecha_creacion, 
         cl.id_cliente, 
         cl.correo AS cliente_correo,
-        cl.nombre AS cliente_nombre, 
-        cl.apellido AS cliente_apellido, 
+        cl.nombre_completo AS cliente_nombre_completo, 
         cl.telefono AS cliente_telefono, 
         m.id_manicurista, 
         m.correo AS manicurista_correo,
-        m.nombre AS manicurista_nombre, 
-        m.apellido AS manicurista_apellido, 
-        m.telefono AS manicurista_telefono, 
-        a.id_administrativo, 
-        a.correo AS admin_correo,
-        a.nombre AS admin_nombre, 
-        a.apellido AS admin_apellido, 
-        a.telefono AS admin_telefono
+        m.nombre_completo AS manicurista_nombre_completo, 
+        m.telefono AS manicurista_telefono
       FROM citas c
       JOIN clientes cl ON c.id_cliente = cl.id_cliente
       JOIN manicuristas m ON c.id_manicurista = m.id_manicurista
-      JOIN administrativos a ON c.gestiona_administrativo = a.id_administrativo
       WHERE c.id_cita = ?;
     `;
