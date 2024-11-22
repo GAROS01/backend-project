4d22e442aa24e071b67c1408b878607160afbecc
GAROSDEV <garzonoscar7@gmail.com>
2024-11-22 17:24:44 -0500

    Refactor estructura del servidor: mover configuración de Express a app.js

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import clientesRoutes from "./routes/clientes.routes.js";
+import adminRoutes from "./routes/administrativos.routes.js";
+import manicuristasRoutes from "./routes/manicuristas.routes.js";
+import citasRoutes from "./routes/citas.routes.js";
+import userValidation from "./routes/validate-user.routes.js";
+import validateCita from "./routes/validate-client-cita.js";
+
+const app = express();
+app.use(morgan("tiny"));
+app.use(cors());
+
+app.use(express.json());
+app.use("/api", clientesRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", manicuristasRoutes);
+app.use("/api", citasRoutes);
+app.use("/api", userValidation);
+app.use("/api", validateCita);
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Not found",
+  });
+});
+
+morgan(":method :url :status :res[content-length] - :response-time ms");
+
+export default app;
