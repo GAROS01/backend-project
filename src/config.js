ab1cfd0fcbdd2826621519eaedbbea98cef68bcc
GAROSDEV <garzonoscar7@gmail.com>
2024-11-22 17:15:18 -0500

    Actualizar variable de entorno para la base de datos

--- src/config.js
+++ src/config.js
@@ -7,4 +7,4 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
 export const DB_PORT = process.env.DB_PORT || 3307;
-export const DB_DATABASE = process.env.DB_DATABASE || "jobdb";
+export const DB_DATABASE = process.env.DB_NAME || "jobdb";
